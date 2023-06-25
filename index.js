e47ad2c34b7b3d3347378a6583b8038799918946
Gd2010 <128714689+dacoder123456m@users.noreply.github.com>
2023-06-25 09:14:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
- // Add this JavaScript code to prevent reload on scroll in Safari
-      (function() {
-        var isScrolling;
-        var scrollPosition = window.pageYOffset;
+// Add this JavaScript code to prevent reload on scroll in Safari
+(function() {
+  var isScrolling;
+  var scrollPosition = window.pageYOffset;
 
-        // Listen for the scroll event
-        window.addEventListener('scroll', function() {
-          // Clear the timeout if it has already been set
-          window.clearTimeout(isScrolling);
+  // Listen for the scroll event
+  window.addEventListener('scroll', function() {
+    // Clear the timeout if it has already been set
+    window.clearTimeout(isScrolling);
 
-          // Set a timeout to run after scrolling ends
-          isScrolling = setTimeout(function() {
-            // Add the overscroll-behavior property back after scrolling ends
-            document.body.style.overscrollBehavior = 'auto';
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function() {
+      // Add the overscroll-behavior property back after scrolling ends
+      document.body.style.overscrollBehavior = 'auto';
 
-            // Store the new scroll position
-            scrollPosition = window.pageYOffset;
-          }, 100);
+      // Store the new scroll position
+      scrollPosition = window.pageYOffset;
+    }, 100);
 
-          // Disable the overscroll-behavior property when scrolling starts
-          document.body.style.overscrollBehavior = 'none';
+    // Disable the overscroll-behavior property when scrolling starts
+    document.body.style.overscrollBehavior = 'none';
 
-          // Check if scrolling back to the top and restore overscroll-behavior property
-          if (window.pageYOffset === 0 && scrollPosition !== 0) {
-            document.body.style.overscrollBehavior = 'auto';
-          }
-        }, false);
-      })();
+    // Check if scrolling back to the top and restore overscroll-behavior property
+    if (window.pageYOffset === 0 && scrollPosition !== 0) {
+      document.body.style.overscrollBehavior = 'auto';
+    }
+  }, false);
+})();
 
-// Get the current day
-      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      var today = new Date();
-      var currentDay = days[today.getDay()];
+// Get all the video elements
+var videos = document.getElementsByClassName("dropdown-toggle");
 
-      // Display an alert with the current day
-      alert('Today is ' + currentDay);
+// Loop through the video elements and attach click event listeners
+for (var i = 0; i < videos.length; i++) {
+  videos[i].addEventListener("click", function() {
+    // Find the closest parent element with class "dropdown" to get the video URL
+    var dropdown = this.closest(".dropdown");
+    var videoUrl = dropdown.querySelector("iframe").src;
 
-// Get all the dropdown toggle elements
-var dropdownToggle = document.getElementsByClassName("dropdown-toggle");
-
-// Loop through the toggle elements and attach click event listeners
-for (var i = 0; i < dropdownToggle.length; i++) {
-  dropdownToggle[i].addEventListener("click", function() {
-    // Toggle the "active" class on the parent element (dropdown container)
-    this.parentElement.classList.toggle("active");
+    // Add your logic for handling the click event on videos here
+    // For example, you can play or pause the video, open a modal, or navigate to a new page
+    alert("Video clicked! URL: " + videoUrl);
   });
 }
 
