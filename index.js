1d26ee9f348fd1d8526d80469e0453cbbc36f171
Gd2010 <128714689+dacoder123456m@users.noreply.github.com>
2023-06-25 09:16:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Get all the video elements
+var videos = document.getElementsByClassName("dropdown-toggle");
+
+// Loop through the video elements and attach click event listeners
+for (var i = 0; i < videos.length; i++) {
+  videos[i].addEventListener("click", function() {
+    // Find the closest parent element with class "dropdown" to get the video URL
+    var dropdown = this.closest(".dropdown");
+    var videoUrl = dropdown.querySelector("iframe").src;
+
+    // Open the video URL in a new page
+    window.open(videoUrl, "_blank");
+  });
+}
+
 // Add this JavaScript code to prevent reload on scroll in Safari
 (function() {
   var isScrolling;
@@ -26,20 +41,4 @@
     }
   }, false);
 })();
-
-// Get all the video elements
-var videos = document.getElementsByClassName("dropdown-toggle");
-
-// Loop through the video elements and attach click event listeners
-for (var i = 0; i < videos.length; i++) {
-  videos[i].addEventListener("click", function() {
-    // Find the closest parent element with class "dropdown" to get the video URL
-    var dropdown = this.closest(".dropdown");
-    var videoUrl = dropdown.querySelector("iframe").src;
-
-    // Add your logic for handling the click event on videos here
-    // For example, you can play or pause the video, open a modal, or navigate to a new page
-    alert("Video clicked! URL: " + videoUrl);
-  });
-}
 
