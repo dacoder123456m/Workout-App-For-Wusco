0914bcebc4659f954127885f6a6eabd9d19c527a
Gd2010 <128714689+dacoder123456m@users.noreply.github.com>
2023-06-25 20:40:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,42 +38,29 @@ document.addEventListener("DOMContentLoaded", function() {
       // Prevent the default behavior of the button
       event.preventDefault();
 
-      // Find the closest parent element with class "day" to calculate the center position
-      var day = this.closest(".day");
-
-      // Find the dropdown content element
+      // Find the closest parent element with class "dropdown" to activate the dropdown menu
       var dropdown = this.parentNode.querySelector(".dropdown-content");
-
-      // Calculate the horizontal center position of the day div
-      var dayCenter = day.getBoundingClientRect().left + day.offsetWidth / 2;
-
-      // Calculate the left position of the dropdown content
-      var dropdownLeft = dayCenter - dropdown.offsetWidth / 2;
-
-      // Set the left position of the dropdown content
-      dropdown.style.left = dropdownLeft + "px";
-
-      // Toggle the visibility of the dropdown content
       dropdown.classList.toggle("show");
+
+      // Center the dropdown horizontally within the day div
+      centerDropdown(dropdown);
     });
+  });
+
+  // Center the dropdown on page load (if open)
+  var openDropdowns = document.querySelectorAll(".dropdown-content.show");
+  openDropdowns.forEach(function(dropdown) {
+    centerDropdown(dropdown);
   });
 });
 
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Get all the dropdown toggle elements
-  var dropdownToggles = document.getElementsByClassName("dropdown-toggle");
+// Function to center the dropdown horizontally within the day div
+function centerDropdown(dropdown) {
+  var dayDiv = dropdown.closest(".day");
+  var dayDivWidth = dayDiv.offsetWidth;
+  var dropdownWidth = dropdown.offsetWidth;
+  var dropdownLeft = (dayDivWidth - dropdownWidth) / 2;
 
-  // Attach event listeners to each dropdown toggle
-  for (var i = 0; i < dropdownToggles.length; i++) {
-    dropdownToggles[i].addEventListener("click", function() {
-      // Toggle the display of the next sibling (dropdown content)
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "none") {
-        dropdownContent.style.display = "block";
-      } else {
-        dropdownContent.style.display = "none";
-      }
-    });
-  }
-});
+  dropdown.style.left = dropdownLeft + "px";
+}
+
